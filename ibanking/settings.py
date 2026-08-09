a3633d07ac8161e26a14e606f1831463c6c01584
Kavita Singh Panwar <kavitasinghpanwar33@gmail.com>
2026-08-10 00:20:35 +0530

    Fix email backend for Render

--- ibanking/settings.py
+++ ibanking/settings.py
@@ -84,16 +84,12 @@ DATABASES = {
 # ---------------- EMAIL CONFIG ---------------- #
 
 
-if os.environ.get('RENDER'):
-    # Render Free blocks outbound SMTP connections
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-else:
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_PORT = 587
-    EMAIL_USE_TLS = True
-    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
